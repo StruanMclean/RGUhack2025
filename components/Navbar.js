e1560c5c64f8ae43f01e85738c0c38a1d795ea4a
struan <struanmclean16@gmail.com>
2025-02-22 22:10:38 +0000

    Navbar Fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,33 @@
+import { Center } from '@mantine/core';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <div style={styles.navbar}>
-            <nav>
-                <div style={styles.container}>
-                    <ul style={styles.navList}>
-                        <li style={styles.navItem}>
-                            <a href="/gallery" style={styles.navLink}>Gallery</a>
-                        </li>
-                        <li style={styles.navItem}>
-                            <a href="/dashboard" style={styles.navLink}>Dashboard</a>
-                        </li>
-                        <li style={styles.navItemCircle}>
-                            <a href="/upload" style={styles.navLink}>+</a>
-                        </li>
-                        <li style={styles.navItem}>
-                            <a href="/settings" style={styles.navLink}>Settings</a>
-                        </li>
-                        <li style={styles.navItem}>
-                            <a href="/settings" style={styles.navLink}>Settings</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <Center>
+            <div style={styles.navbar}>
+                <nav>
+                    <div style={styles.container}>
+                        <ul style={styles.navList}>
+                            <li style={styles.navItem}>
+                                <a href="/gallery" style={styles.navLink}>Gallery</a>
+                            </li>
+                            <li style={styles.navItem}>
+                                <a href="/dashboard" style={styles.navLink}>Dashboard</a>
+                            </li>
+                            <li style={styles.navItemCircle}>
+                                <a href="/upload" style={styles.navLink}>+</a>
+                            </li>
+                            <li style={styles.navItem}>
+                                <a href="/settings" style={styles.navLink}>Settings</a>
+                            </li>
+                            <li style={styles.navItem}>
+                                <a href="/settings" style={styles.navLink}>Settings</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>            
+        </Center>
     );
 };
 
@@ -34,7 +37,7 @@ const styles = {
         padding: '10px 0',
         textAlign: 'center',
         position: 'fixed', 
-        bottom: '0', 
+        bottom: '25px', 
         width: '100%',
     },
     navList: {
@@ -45,7 +48,7 @@ const styles = {
         alignItems: 'center', // Aligns all items (including the circle) vertically
     },
     navItem: {
-        margin: '0 15px',
+        margin: '0 25px',
         textAlign: 'center',
     },
     navItemCircle: {
@@ -57,26 +60,29 @@ const styles = {
         display: 'flex', 
         justifyContent: 'center', 
         alignItems: 'center', // Centers content inside the circle
-        outline: '2px solid black',
+        outline: '10px solid white',
         backgroundColor: 'lightblue',
     },
     navLink: {
         color: 'black',
         textDecoration: 'none',
-        fontSize: '30px',
+        fontSize: '19px',
     },
     container: {
         display: 'flex',
         justifyContent: 'center',
-        backgroundColor: 'lightgrey',
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: "blur(5px)",
+        webkitBackdropFilter: "blur(5px)",
         width: 'fit-content',
         position: 'absolute',
         top: '-100%',
         left: '50%',
-        transform: 'translate(-50%, -50%)', // Proper centering
+        transform: 'translate(-50%, -50%)',
         padding: '10px',
         borderRadius: '10px',
-        height: '50px' // Fixed height declaration
+        height: '50px'
     }
 };
 
