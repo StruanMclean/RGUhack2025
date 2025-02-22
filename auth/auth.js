9030b1f94de7727a29328256ed1181efcd6665f9
struan <struanmclean16@gmail.com>
2025-02-22 20:58:51 +0000

    Auth working

--- auth/auth.js
+++ auth/auth.js
@@ -1,11 +1,11 @@
 'use client'
 import { auth } from './firebase';
-import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword as _createUserWithEmailAndPassword, signInWithEmailAndPassword as _signInWithEmailAndPassword, signOut as _signOut } from "firebase/auth";
-import { useEffect, useState } from "react"
+import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword as _createUserWithEmailAndPassword, signInWithEmailAndPassword as _signInWithEmailAndPassword, signOut as _signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function useAuth() {
-  const [authUser, setAuthUser] = useState()
-  const [loading, setLoading] = useState(true)
+  const [authUser, setAuthUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const clear = () => {
     setAuthUser(null);
@@ -31,6 +31,23 @@ export default function useAuth() {
 
   const signOut = () =>
     _signOut(auth).then(clear);
+
+  // This effect listens to authentication changes
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+        setLoading(false);
+        // Redirect to dashboard when user is logged in
+        window.location.replace("/dashboard")
+      } else {
+        setAuthUser(null);
+        setLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return {
     authUser,
