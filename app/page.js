98e013ac8c1002e6a94e18753ad4be16650bfff5
struan <struanmclean16@gmail.com>
2025-02-22 18:03:53 +0000

    init

---
+++ app/page.js
@@ -0,0 +1,34 @@
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import classes from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius={0} p={30}>
+        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+          Welcome back to Mantine!
+        </Title>
+
+        <TextInput label="Email address" placeholder="hello@gmail.com" size="md" />
+        <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" />
+        <Checkbox label="Keep me logged in" mt="xl" size="md" />
+        <Button fullWidth mt="xl" size="md">
+          Login
+        </Button>
+
+        <Text ta="center" mt="md">
+          Don&apos;t have an account?{' '}
+        </Text>
+      </Paper>
+    </div>
+  );
+}
---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<body>
+    <div class=" " id="Login">
+        <div id="Carousel">
+            <img src="Test.png" alt="">
+        </div>
+        <div id="Form">
+            <h1>Create An Account Today</h1>
+            <h2>Already have an account? <a href="#">Log in</a></h2>
+            
+            <form action="">
+                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="password" name="password" id="password" placeholder="Password">
+                <input type="checkbox">Agree to terms and conditions   
+                <input class=" btn" type="submit" value="Create Account">
+            </form>
+
+            <button class="btn">hello world</button>
+        </div>
+    </div>
+</body>
