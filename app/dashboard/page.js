b4e5ca991183c2982074d4dbe122421ed6188b3c
Jayden-Wilson-01 <jayden@tabber.co.uk>
2025-02-22 18:55:06 +0000

    dash

---
+++ app/dashboard/page.js
@@ -0,0 +1,35 @@
+import {
+    Anchor,
+    Button,
+    Checkbox,
+    Paper,
+    PasswordInput,
+    Text,
+    TextInput,
+    Title,
+  } from '@mantine/core';
+  import classes from './page.module.css';
+  
+  export default function Dashboard() {
+    return (
+      <div className={classes.wrapper}>
+        <Paper className={classes.form} radius={0} p={30}>
+          <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+            Dashboard
+          </Title>
+  
+          <TextInput label="Email address" placeholder="YourEmail@gmail.com" size="md" />
+          <PasswordInput label="Password" placeholder="Your Password" mt="md" size="md" />
+          <Checkbox label="Keep me logged in" mt="xl" size="md" />
+          <Button fullWidth mt="xl" size="md">
+            
+            Login
+          </Button>
+  
+          <Text ta="center" mt="md">
+            Don&apos;t have an account?{' '}
+          </Text>
+        </Paper>
+      </div>
+    );
+  }
