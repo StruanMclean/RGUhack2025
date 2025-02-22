d353656a8a59e087d7f9ba759cc73db6007c4b8b
struan <struanmclean16@gmail.com>
2025-02-22 20:12:41 +0000

    useAuth

--- auth/auth.js
+++ auth/auth.js
@@ -1,14 +1,48 @@
 'use client'
 import { auth } from './firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
-export const signInWithGoogle = async () => {
-  const provider = new GoogleAuthProvider();
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    console.log(user);
-  } catch (error) {
-    console.error(error);
-  }
-};
+export default function useAuth() {
+  const clear = () => {
+    setAuthUser(null);
+    setLoading(true);
+  };
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log(user);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const signInWithEmailAndPassword = (email, password) =>
+    _signInWithEmailAndPassword(auth, email, password);
+
+  const createUserWithEmailAndPassword = (email, password) =>
+    _createUserWithEmailAndPassword(auth, email, password);
+
+  const signOut = () =>
+    _signOut(auth).then(clear);
+
+  const onAuthStateChanged = (cb) => {
+      return _onAuthStateChanged(auth, cb);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(authStateChanged);
+    return () => unsubscribe();
+  }, []);
+
+  return {
+    authUser,
+    loading,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    signInWithGoogle
+  };
+}
