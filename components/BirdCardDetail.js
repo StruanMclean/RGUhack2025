1e293606c056b34d36664e23804e91cf912d284f
Jayden-Wilson-01 <jayden@tabber.co.uk>
2025-02-22 23:04:53 +0000

    bird card detail

--- components/BirdCardDetail.js
+++ components/BirdCardDetail.js
@@ -1,28 +1,19 @@
-'use client'
-import { useState } from 'react';
+// Card.js
+import React from 'react';
 import classes from './BirdCard.module.css';  // Optional, for styling the card
 import { Card } from '@mantine/core';
-import BirdCardDetail from './BirdCardDetail';
 
-const BirdCard = ({ image, title, fact, date }) => {
-  const [showOverlay, setShowOverlay] = useState(false)
+const BirdCardDetail = ({ image, title, fact, date }) => {
   return (
-    <Card className={classes.birdcard} onClick={() => setShowOverlay(true)}>
+    <Card className={classes.birdcard}>
       <div>
         <img src={image} alt={title} className={classes.cardimage} />
         <h4 className={classes.cardtitle}>{title}</h4>
         <p className={classes.cardfact}>{fact}</p>
         <p className={classes.carddate}>{date}</p>
       </div>
-
-      {
-        showOverlay ?
-          <BirdCardDetail />
-        :
-          <></>
-      }
     </Card>
   );
 };
 
-export default BirdCard;
+export default BirdCardDetail;
