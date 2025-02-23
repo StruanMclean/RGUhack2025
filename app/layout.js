3161a1ca696e08a9c2a9f4bf9dcdd0bd7862bcfc
struan <struanmclean16@gmail.com>
2025-02-23 11:48:29 +0000

    env

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
         <head>
-          <meta name="viewport" content="width=750px, initial-scale=1, maximum-scale=1, user-scalable=no" />
+          <meta name="viewport" content="width=900px, initial-scale=1, maximum-scale=1, user-scalable=no" />
         </head>
         <body>
           <MantineProvider>
