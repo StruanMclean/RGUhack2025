738be97cf9f0a5ec0ac49ba33d1814fd07f1aa90
struan <struanmclean16@gmail.com>
2025-02-23 01:46:54 +0000

    Database

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,6 +4,8 @@ import {
   Paper,
   Flex,
   Center,
+  Title,
+  Button,
   } from '@mantine/core';
   import classes from './page.module.css';
 import BirdCard from '../../components/BirdCard';
@@ -11,20 +13,10 @@ import Navbar from '../../components/Navbar';
 
 import Map, {Source, Layer} from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { auth, firestore } from '../../auth/firebase';
+import { useEffect, useState } from 'react';
 
-const geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-2.29895, 57.1523021]
-      },
-      properties: {title: '26 westdyke avenue'}
-    }
-  ]
-};
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore"; 
 
 const layerStyle = {
   id: 'point',
@@ -37,15 +29,56 @@ const layerStyle = {
   
 export default function Dashboard() {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+  const [items, setItems] = useState([]);
+  const [geojson, setgeojson] = useState([]);
+  const [update, setUpdate] = useState(true);
+
+  const handleDelete = async (id) => {
+    const itemRef = doc(firestore, "Uploads", id)
+    try {
+      await deleteDoc(itemRef)
+      setUpdate(update ? false : true)
+    } catch (error) {
+      console.error("Error deleting document: ", error)
+      alert("Error deleting item")
+    }
+  }
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "Uploads"));
+        setItems(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setgeojson(
+          querySnapshot.docs.map((doc) => ({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [doc.data().longitude, doc.data().latitude]
+            },
+            properties: {title: 'data'}
+          }))
+        );
+  
+        console.log(querySnapshot);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
+    };
+  
+    fetchItems();
+  }, [update]);  
 
   return (
     <div  className={classes.wrapper}>
       <Grid>
         <Grid.Col span={6}>
           <Paper className={classes.form} radius={0} p={30}>
-            <h1 order={2} className={classes.title} ta="center" mt="md" mb={50}>
-              Dashboard
-            </h1>
+            <Center>
+              <Title>
+                Welcome Back
+              </Title>              
+            </Center>
 
             <Center>
             <h2 className = {classes.subtitle}>Recents</h2>
@@ -53,9 +86,12 @@ export default function Dashboard() {
 
             <Center>
               <Flex>
-                <BirdCard image={"wood-flight-bird-326900.jpg"} title={"Bird 1"} fact={"Fact 1"} date={"22/02/2025"}></BirdCard>
-                <BirdCard image={"wood-flight-bird-326900.jpg"} title={"Bird 2"} fact={"Fact 2"} date={"22/02/2025"}></BirdCard>
-                <BirdCard image={"wood-flight-bird-326900.jpg"} title={"Bird 3"} fact={"Fact 3"} date={"22/02/2025"}></BirdCard>
+                {items.map((item) => (
+                  <li key={item.id} className="border-t-2 p-2">
+                    <BirdCard image={"wood-flight-bird-326900.jpg"} title={item.fileName} fact={"Fact 3"} date={"22/02/2025"}></BirdCard>
+                    <Button onClick={() => handleDelete(item.id)}>Delete</Button>
+                  </li>
+                ))}
               </Flex>
             </Center>
 
@@ -71,7 +107,12 @@ export default function Dashboard() {
             maxZoom={20}
             minZoom={3}
           >
-            <Source id="my-data" type="geojson" data={geojson}>
+            <Source id="my-data" type="geojson" data={
+              {
+                type: 'FeatureCollection',
+                features: geojson
+              }
+            }>
               <Layer {...layerStyle} />
             </Source>
           </Map>
--- app/upload/page.js
+++ app/upload/page.js
@@ -1,20 +1,58 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { IconCloudUpload, IconDownload, IconX } from '@tabler/icons-react';
-import { Button, Group, Text, useMantineTheme } from '@mantine/core';
+import { Button, Card, Group, Text, useMantineTheme, Center, Title, TextInput, Container } from '@mantine/core';
 import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
 import classes from './page.module.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import toast from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
+import { auth, firestore } from '../../auth/firebase';
+import { collection, addDoc } from "firebase/firestore"; 
 
 export default function Upload() {
   const theme = useMantineTheme();
   const openRef = useRef(null);
 
+  const [fileName, setFileName] = useState("")
+
+  const submit = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          try {
+            const docRef = await addDoc(collection(firestore, "Uploads"), {
+              fileName: fileName,
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            });
+            console.log("Upload successful:", docRef);
+          } catch (error) {
+            console.error("Error uploading data:", error);
+            alert("Error during upload");
+          }
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+          toast.error("Could not access your location", {
+            position: "top-center",
+          });
+        }
+      );
+    } else {
+      alert("Error: Geolocation not available");
+      toast.error("Could not access your location to add to the map", {
+        position: "top-center",
+      });
+    }
+  };
+  
 
   return (
     <>
+      <Toaster />
       <div className={classes.wrapper}>
         <Dropzone
           openRef={openRef}
@@ -24,7 +62,25 @@ export default function Upload() {
           accept={[MIME_TYPES.pdf]}
           maxSize={30 * 1024 ** 2}
         >
-          <div style={{ pointerEvents: 'none' }}>
+          <Center mt={100}>
+            <Title>
+              Upload Bird
+            </Title>              
+          </Center>
+
+          <div style={{
+            marginTop: "3cm",
+            width: 500,
+            padding: 50,
+            borderRadius: 20,
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(5px)",
+            webkitBackdropFilter: "blur(5px)",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 50,
+          }}>
             <Group justify="center">
               <Dropzone.Accept>
                 <IconDownload size={50} color={theme.colors.blue[6]} stroke={1.5} />
@@ -42,17 +98,31 @@ export default function Upload() {
               <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
               <Dropzone.Idle>Upload resume</Dropzone.Idle>
             </Text>
-            <Text ta="center" fz="sm" mt="xs" c="dimmed">
+            <Text ta="center" fz="sm" mt="xs">
               Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
               are less than 30mb in size.
             </Text>
+
+            <Button className={classes.control} size="md" radius="xl" w={400} mt={50} onClick={() => openRef.current?.()}>
+              Select files
+            </Button>
           </div>
         </Dropzone>
-
-        <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
-          Select files
-        </Button>
       </div>
+
+      <Container maw={500}>
+        <TextInput 
+          value={fileName}
+          onChange={(event) => setFileName(event.currentTarget.value)}
+          label="File Name" 
+          placeholder="File Name" 
+          size="md" 
+        />
+
+        <Button size="md" fullWidth mt={25} onClick={() => submit()}>
+          Submit
+        </Button>
+      </Container>
 
       <Navbar />
       <Footer />
