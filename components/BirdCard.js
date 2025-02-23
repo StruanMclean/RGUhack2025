a1ce9a5ee64fef7a784da899405120232ea9151b
struan <struanmclean16@gmail.com>
2025-02-23 00:13:48 +0000

    button

--- components/BirdCard.js
+++ components/BirdCard.js
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import classes from './BirdCard.module.css';  // Optional, for styling the card
 import { Card } from '@mantine/core';
 import BirdCardDetail from './BirdCardDetail';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button } from '@mantine/core';
 
 const BirdCard = ({ image, title, fact, date }) => {
-  const [showOverlay, setShowOverlay] = useState(false)
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <Card className={classes.birdcard} onClick={() => setShowOverlay(true)}>
+    <Card className={classes.birdcard} onClick={open}>
       <div>
         <img src={image} alt={title} className={classes.cardimage} />
         <h4 className={classes.cardtitle}>{title}</h4>
@@ -15,12 +18,9 @@ const BirdCard = ({ image, title, fact, date }) => {
         <p className={classes.carddate}>{date}</p>
       </div>
 
-      {
-        showOverlay ?
-          <BirdCardDetail />
-        :
-          <></>
-      }
+      <Modal opened={opened} onClose={close} title="Authentication">
+        <BirdCardDetail />
+      </Modal>
     </Card>
   );
 };
